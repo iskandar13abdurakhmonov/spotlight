02d2ab401912c250db8eb5b0c651018692761c24
eescan <iskandar.13.aburahmonov@gmail.com>
2025-08-08 07:03:09 +0500

    create post mutation

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,52 @@
-import {Text, View} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
+import {useRouter} from "expo-router";
+import {useUser} from "@clerk/clerk-expo";
+import {useState} from "react";
+import {styles} from "@/styles/create.styles";
+import {Ionicons} from "@expo/vector-icons";
+import {COLORS} from "@/constants/theme";
 
-export default function Create() {
+import * as ImagePicker from "expo-image-picker"
+
+export default function CreateScreen() {
+
+    const router = useRouter()
+    const user = useUser()
+
+    const [caption, setCaption] = useState("")
+    const [selectedImage, setSelectedImage] = useState<string | null>(null)
+    const [isSharing, setIsSharing] = useState(false)
+
+    const pickImage = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: "images",
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.8
+        })
+
+        if(!result.canceled) setSelectedImage(result.assets[0].uri)
+    }
+
+    if(!selectedImage) {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => router.back()}>
+                        <Ionicons name='arrow-back' size={28} color={COLORS.primary}/>
+                    </TouchableOpacity>
+                    <Text style={styles.headerTitle}>New Post</Text>
+                    <View style={{ width: 28 }}/>
+                </View>
+
+                <TouchableOpacity style={styles.emptyImageContainer} onPress={pickImage}>
+                    <Ionicons name='image-outline' size={48} color={COLORS.grey}/>
+                    <Text style={styles.emptyImageText}>Tap to select an image</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <View>
             <Text>Create</Text>
